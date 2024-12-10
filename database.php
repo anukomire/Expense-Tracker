3e154f05cc925889299beeacb66fe75872ba379b
Komire Anusha <103433381+anukomire@users.noreply.github.com>
2024-12-10 23:02:24 +0530

    Database connection

---
+++ database.php
@@ -0,0 +1,14 @@
+<?php
+$host = 'localhost';  // Database host
+$dbname = 'expensetracker'; // Database name
+$username = 'root';   // Database username
+$password = '';       // Database password (default for XAMPP)
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo 'Database connection failed: ' . $e->getMessage();
+    exit;
+}
+?>
